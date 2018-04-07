d07480097b542ce3c2c7f7c0e880306334af7bf8
T. Michael Keesey <keesey@gmail.com>
2018-04-06 22:42:55 -0700

    Added react-redux.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import { registerServiceWorker } from "./registerServiceWorker";
-ReactDOM.render(
-	<App />,
+import store from "./store";
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.getElementById("root") as HTMLElement,
 );
 registerServiceWorker();
