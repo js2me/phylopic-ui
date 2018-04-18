1161598dcc53c59040109b4d618731b91bc9c1f3
T. Michael Keesey <keesey@gmail.com>
2018-04-17 21:55:44 -0700

    Moved most window size functionality into helpers.

--- src/features/windowSize/index.tsx
+++ src/features/windowSize/index.tsx
@@ -1,28 +1,8 @@
 import * as React from "react";
-import { getWindowSize } from "../../helpers/windowSize";
+import { getWindowSize, listenToWindowResize } from "../../helpers/windowSize";
 export interface DispatchProps {
 	onResize: (size: [number, number]) => void;
 }
-declare global {
-	interface Window {
-		attachEvent: (name: string, listener: (event: Event) => void) => void;
-	}
-}
-const listenToWindowResize = (listener: () => void) => {
-	if (window.addEventListener) {
-		window.addEventListener("resize", listener);
-	} else if (window.attachEvent) {
-		window.attachEvent("onresize", listener);
-	} else if (window.onresize) {
-		const existing = window.onresize;
-		window.onresize = (event: UIEvent) => {
-			listener();
-			existing.call(window, event);
-		};
-	} else {
-		window.onresize = listener;
-	}
-};
 class WindowSize extends React.Component<DispatchProps> {
 	public componentWillMount() {
 		listenToWindowResize(() => this.props.onResize(getWindowSize()));
--- src/helpers/windowSize.ts
+++ src/helpers/windowSize.ts
@@ -1,3 +1,8 @@
+declare global {
+	interface Window {
+		attachEvent: (name: string, listener: (event: Event) => void) => void;
+	}
+}
 export const getWindowSize: () => [number, number] = () => {
 	const element = document.documentElement;
 	const body = document.getElementsByTagName("body")[0];
@@ -5,4 +10,19 @@ export const getWindowSize: () => [number, number] = () => {
 		window.innerWidth || element.clientWidth || body.clientWidth,
 		window.innerHeight || element.clientHeight || body.clientHeight,
 	];
+};
+export const listenToWindowResize = (listener: () => void) => {
+	if (window.addEventListener) {
+		window.addEventListener("resize", listener);
+	} else if (window.attachEvent) {
+		window.attachEvent("onresize", listener);
+	} else if (window.onresize) {
+		const existing = window.onresize;
+		window.onresize = (event: UIEvent) => {
+			listener();
+			existing.call(window, event);
+		};
+	} else {
+		window.onresize = listener;
+	}
 };
